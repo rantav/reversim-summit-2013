06f9f921891a575f0b89ee575c575068df34c38a
Ran Tavory <rantav@gmail.com>
2012-12-03 23:45:28 +0200

    improve the deploy script such that if there are no forgotten files, it doesn't quit

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 set -e
 yeoman build
 git add .
+set +e
 git ci -am "Add all forgotten files..."
+set -e
 git co gh-pages
 rm dist/dist
 cp -r dist/* .
+set +e
 git add .
 git ci -am "another day another page"
+set -e
 git push
