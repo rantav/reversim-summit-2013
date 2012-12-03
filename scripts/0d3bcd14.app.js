80429fa9b3a32ac66d4b3302532cee7d3037958e
Ran Tavory <rantav@gmail.com>
2012-12-03 23:47:15 +0200

    another day another page

---
+++ scripts/0d3bcd14.app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var reversimSummit2013App = angular.module('reversimSummit2013App', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
