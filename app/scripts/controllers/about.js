cdd8bced565d4c2671e3f478fcd7c0655f7a2f35
Ran Tavory <rantav@gmail.com>
2012-12-05 20:59:56 +0200

    Add images or Ori and Ran and add Twitter bootstrap back again

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,4 +1,16 @@
 'use strict';
 
 reversimSummit2013App.controller('AboutCtrl', ['$scope',function($scope) {
+  $scope.people = [
+    {
+      'name': 'Ran Tavory',
+      'image': 'ran-tavory.png',
+      'bio': 'TODO'
+    },
+    {
+      'name': 'Ori Lahav',
+      'image': 'ori-lahav.jpeg',
+      'bio': 'TODO'
+    }
+  ];
 }]);
