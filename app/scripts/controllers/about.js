8d9c4f3974a7299cc865f6116baa85bb4f8287b3
Ran Tavory <rantav@gmail.com>
2012-12-05 23:45:01 +0200

    Make the images of Ori and Ran look good. And add bio for them

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -5,12 +5,12 @@ reversimSummit2013App.controller('AboutCtrl', ['$scope',function($scope) {
     {
       'name': 'Ran Tavory',
       'image': 'ran-tavory.png',
-      'bio': 'TODO'
+      'bio': "Ran is a full stack developer and The Voice™, at reversim. Ran's interests are in slick and responsive web interfaces, iOS, nosql, web infrastructure, scaling, deployment automation, continuous deployment, programming languages, open source enthusiast (a contributor and creator of several projects). Ran is a drummer with a small band, a husband and a father of two"
     },
     {
       'name': 'Ori Lahav',
       'image': 'ori-lahav.jpeg',
-      'bio': 'TODO'
+      'bio': "Ori Lahav is a Co-founder and CTO of outbrain and The Voice™ at reversim. Ori like paddling his kayak through rough seas and sail a yacht when storms are out.            "
     }
   ];
 }]);
