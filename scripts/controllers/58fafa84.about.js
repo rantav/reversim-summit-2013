1cc9e74fa1e84d26af7a365acd2c9b8046e2fd3f
Ran Tavory <rantav@gmail.com>
2012-12-06 00:25:02 +0200

    add about us with images

---
+++ scripts/controllers/58fafa84.about.js
@@ -0,0 +1,18 @@
+'use strict';
+
+reversimSummit2013App.controller('AboutCtrl', ['$scope',function($scope) {
+  $scope.people = [
+    {
+      'name': 'Ran Tavory',
+      'image': 'ran-tavory.png',
+      'image_class': 'ran-tavory',
+      'bio': "Ran is a full stack developer and The Voice™, at reversim. Ran's interests are in slick and responsive web interfaces, iOS, nosql, web infrastructure, scaling, deployment automation, continuous deployment, programming languages, open source enthusiast (a contributor and creator of several projects). Ran is a drummer with a small band, a husband and a father of two"
+    },
+    {
+      'name': 'Ori Lahav',
+      'image': 'ori-lahav.jpeg',
+      'image_class': 'ori-lahav',
+      'bio': "Ori Lahav is a Co-founder and CTO of outbrain and The Voice™ at reversim. Ori like paddling his kayak through rough seas and sail a yacht when storms are out.            "
+    }
+  ];
+}]);
