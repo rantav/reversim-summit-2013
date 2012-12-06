ed1c2aa347a1f32eb4cc25e246ef691a21d960af
Ran Tavory <rantav@gmail.com>
2012-12-06 23:43:11 +0200

    Fix some images links in the css and work on improved caching

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -4,14 +4,14 @@ reversimSummit2013App.controller('AboutCtrl', ['$scope',function($scope) {
   $scope.people = [
     {
       'name': 'Ran Tavory',
-      'image': 'ran-tavory.png',
-      'image_class': 'ran-tavory',
+      'image': 'ran.png',
+      'image_class': 'ran',
       'bio': "Ran is a full stack developer and The Voice™, at reversim. Ran's interests are in slick and responsive web interfaces, iOS, nosql, web infrastructure, scaling, deployment automation, continuous deployment, programming languages, open source enthusiast (a contributor and creator of several projects). Ran is a drummer with a small band, a husband and a father of two"
     },
     {
       'name': 'Ori Lahav',
-      'image': 'ori-lahav.jpeg',
-      'image_class': 'ori-lahav',
+      'image': 'ori.jpeg',
+      'image_class': 'ori',
       'bio': "Ori Lahav is a Co-founder and CTO of outbrain and The Voice™ at reversim. Ori like paddling his kayak through rough seas and sail a yacht when storms are out.            "
     }
   ];
