94f4a9e78c0183fa9526445ef5094fd56f1c0ba2
Ran Tavory <rantav@gmail.com>
2012-12-09 01:33:12 +0200

    Start reading data from google spreadsheet. It's still lame, but sort of works...

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,18 +1,31 @@
 'use strict';
 
-reversimSummit2013App.controller('AboutCtrl', ['$scope',function($scope) {
-  $scope.people = [
-    {
-      'name': 'Ran Tavory',
-      'image': 'ran.png',
-      'image_class': 'ran',
-      'bio': "Ran is a full stack developer and The Voice™, at reversim. Ran's interests are in slick and responsive web interfaces, iOS, nosql, web infrastructure, scaling, deployment automation, continuous deployment, programming languages, open source enthusiast (a contributor and creator of several projects). Ran is a drummer with a small band, a husband and a father of two"
-    },
-    {
-      'name': 'Ori Lahav',
-      'image': 'ori.jpeg',
-      'image_class': 'ori',
-      'bio': "Ori Lahav is a Co-founder and CTO of outbrain and The Voice™ at reversim. Ori like paddling his kayak through rough seas and sail a yacht when storms are out.            "
+function parseFromSpreatsheet(data, columns) {
+  var entries = data.feed.entry;
+  var ret = [];
+  var regexpString = [];
+  for (var i = 0; i < columns.length; ++i) {
+    regexpString.push(columns[i]+ ": (.*)");
+  }
+  regexpString = regexpString.join(", ");
+  var regexp = new RegExp(regexpString);
+  for (var i = 0; i < entries.length; ++i) {
+    var entry = entries[i];
+    var content = entry.content.$t;
+    var matches = content.match(regexp);
+    var obj = {};
+    for (var m = 1; m < matches.length; ++m) {
+      obj[columns[m - 1]] = matches[m];
     }
-  ];
+    ret.push(obj);
+  }
+  return ret;
+}
+
+reversimSummit2013App.controller('AboutCtrl', ['$scope', '$http',function($scope, $http) {
+  $http.jsonp('http://spreadsheets.google.com/feeds/list/0AngbRXPzHA7adDRoeDFVRkZ1UEY5SXBwSjdSLU1nX2c/1/public/basic?alt=json-in-script&callback=JSON_CALLBACK').success(function(data) {
+    $scope.people = parseFromSpreatsheet(data, ['name', 'css', 'bio']);
+  }).error(function(data) {
+    console.log(data);
+  });
 }]);
