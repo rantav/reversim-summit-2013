9e8f1464a36b25c3b575503299ab01711c5fca6e
Ran Tavory <rantav@gmail.com>
2012-12-09 10:28:59 +0200

    Read the data for the speakers list from google spreadsheets

--- app/scripts/controllers/speakers.js
+++ app/scripts/controllers/speakers.js
@@ -1,17 +1,10 @@
 'use strict';
 
-app.controller('SpeakersCtrl', ['$scope',function($scope) {
-  $scope.people = [
-    {
-      'name': 'Roee Adler',
-      'css': 'roee',
-      'bio': "Roee is the chief product officer of Soluto, a high-tech startup aimed at helping people get more from their technology. Roee started programming at the age of 11. He went on to lead a software group in an elite R&D unit in the Israeli Defense Forces, after which he worked as a device driver developer for Envara, acquired by Intel in 2004. Since then, Roee has managed product design and development in several industries, including: water technology (Miya), location-based services (AeroScout), and music (Neocraft, where Roee was co-founder)."
-    },
-    {
-      'name': 'Yoav Abrahami',
-      'css': 'yoav',
-      'bio': "Working with developers and operations on building wix future products as well as accelerating and improving development processes, Yoav takes the role of Chief Architect. Prior to joining Wix.com, Yoav was an Architect at Amdocs Cramer OSS division. Yoav has a MS in Physics and BS in Computer Science from Tel Aviv University"
-    }
-  ];
-
+app.controller('SpeakersCtrl', ['$scope', '$http', 'data' ,function($scope, $http, data) {
+  var sheet = 2;
+  $http.jsonp(data.getDataSheetUrl(sheet)).success(function(returned) {
+    $scope.people = data.parseFromSpreadsheet(returned, ['name', 'css', 'bio']);
+  }).error(function(returned) {
+    console.log(returned);
+  });
 }]);
