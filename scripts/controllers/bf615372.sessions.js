e0f97d91328e9c3fe0bf4b5c7d0ed468aed4b382
Ran Tavory <rantav@gmail.com>
2012-12-10 22:14:40 +0200

    Add the location of the conf

---
+++ scripts/controllers/bf615372.sessions.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+app.controller('SessionsCtrl', ['$scope', '$http', 'data', '$routeParams' ,function($scope, $http, data, $routeParams) {
+  var sheet = 3;
+  $http.jsonp(data.getDataSheetUrl(sheet)).success(function(returned) {
+    var sessions = data.parseFromSpreadsheet(returned, ['speaker', 'subject', 'track', 'status', 'abstract', 'contact', 'comments']);
+    for (var i = sessions.length - 1; i >= 0; i--) {
+      var s = sessions[i];
+      s['css'] = s.speaker.split(' ')[0].toLowerCase();
+    };
+    $scope.sessions = sessions;
+  }).error(function(returned) {
+    console.log(returned);
+  });
+}]);
