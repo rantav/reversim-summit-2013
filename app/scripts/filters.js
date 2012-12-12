b0308ec022fd50850f57a6bae77dc08790baca9a
Ran Tavory <rantav@gmail.com>
2012-12-12 23:50:48 +0200

    Add docs to the orFilter

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -2,6 +2,12 @@
 
 angular.module('filters', []).
   filter('orFilter', function() {
+    /*
+      An orFilter filters an array of objects by ORing a set of citerias.
+      For example the array [{a: 1}, {b: 2}, {c: 3}, {d: 4}]
+      with the cliterias: [{a: 1}, {b: 2, c: 3}, {d: 4}]
+      would result in: [{a: 1}, {d: 4}]
+     */
     return function(items, orArr) {
       if (!items) {
         return [];
