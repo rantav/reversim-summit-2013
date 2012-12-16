5963f922b591e47ba7e2d633eda3d0cf45bef2fa
Ran Tavory <rantav@gmail.com>
2012-12-17 00:54:20 +0200

    Do not try to use markdown on empty or null string

---
+++ scripts/53d880e9.filters.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('filters', []).
+filter('orFilter', function() {
+  /*
+    An orFilter filters an array of objects by ORing a set of citerias.
+    For example the array [{a: 1}, {b: 2}, {c: 3}, {d: 4}]
+    with the cliterias: [{a: 1}, {b: 2, c: 3}, {d: 4}]
+    would result in: [{a: 1}, {d: 4}]
+   */
+  return function(items, orArr) {
+    if (!items) {
+      return [];
+    }
+    var ret = [];
+    for (var i = items.length - 1; i >= 0; i--) {
+      var item = items[i];
+      for (var j = orArr.length - 1; j >= 0; j--) {
+        var or = orArr[j]
+        for (var p in or) {
+          if (item[p] == or[p]) {
+            ret.push(item);
+            continue;
+          }
+        }
+      };
+    };
+    return ret.reverse();
+  }
+}).
+filter('markdown', function() {
+  return function(input) {
+    return input ? markdown.toHTML(input) : "";
+  };
+});
+
