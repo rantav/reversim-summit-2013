c7d5c045c5068f3d30019399b01dbef7ad339e7d
Ran Tavory <rantav@gmail.com>
2012-12-22 22:23:48 +0200

    Merge the data for the sessions from two sheets: the Sessions sheet and the People sheet

--- app/scripts/controllers/sessions.js
+++ app/scripts/controllers/sessions.js
@@ -1,11 +1,27 @@
 'use strict';
 
+app.controller('SessionsCtrl', ['$scope', '$http', 'data', '$routeParams' ,
+      function($scope, $http, data, $routeParams) {
+  var enrich = function() {
+    if ($scope.people && $scope.sessions) {
+      $scope.sessions = data.enrich($scope.sessions, $scope.people,
+        [['Speaker', 'name']]);
+    }
+  }
 
-app.controller('SessionsCtrl', ['$scope', '$http', 'data', '$routeParams' ,function($scope, $http, data, $routeParams) {
-  var sheet = 3;
-  $http.jsonp(data.getDataSheetUrl(sheet)).success(function(returned) {
+  var sessionsSheet = 3;
+  var peopleSheet = 2;
+  $http.jsonp(data.getDataSheetUrl(sessionsSheet)).success(function(returned) {
     var sessions = data.parseFromSpreadsheet(returned);
     $scope.sessions = sessions;
+    enrich();
+  }).error(function(returned) {
+    console.log(returned);
+  });
+  $http.jsonp(data.getDataSheetUrl(peopleSheet)).success(function(returned) {
+    var people = data.parseFromSpreadsheet(returned);
+    $scope.people = people;
+    enrich();
   }).error(function(returned) {
     console.log(returned);
   });
