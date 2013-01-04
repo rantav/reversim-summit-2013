1c558bc32a07b1b29d53f405a74301c3dc76ad56
Amir Lazarovich <amirlazarovich@gmail.com>
2013-01-04 17:16:08 +0200

    Android-ADK Lab by Amir Lazarovich

---
+++ amir_lazarovich_lab_from_virtual_to_physical.mk
@@ -0,0 +1,21 @@
+## Amir Lazarovich
+
+### Lab Title
+Bridging the gap between the virtual and physical world with Android.
+
+### Abstract
+Introducing the Android Development Kit (adk) and showing developers how easy it is to control physical objects with your Android device.
+
+### Target Audience
+Android developers interested in crossing the boundaries of virtual software development and learning how to hack and control physical objects.
+
+### Requirements
+1. An Android device running 2.3.4 or higher
+2. USB cable
+3. Laptop with:
+ - [Android SDK (including Platform 2.3.3, API Level 10 or higher)](http://developer.android.com/sdk/index.html)
+ - [Arduino IDE & libs (including the Servo library) - follow the entire instructions](http://developer.android.com/tools/adk/adk.html#getting-started)
+ - [Eclipse](http://eclipse.org/mobile/) + [Android plugin](http://developer.android.com/sdk/installing/installing-adt.html)/[Intellij](http://www.jetbrains.com/idea/download/)/etc.
+
+### References
+[About me](http://amirl.me/about.html)
